6b04e15fd69058694a6ca8a3bdfc1ef985dd2a53
MorseTheCode <morsethecode@gmail.com>
2024-10-13 02:45:06 -0300

    Update finish-matches.js

--- scripts/finish-matches.js
+++ scripts/finish-matches.js
@@ -13,13 +13,13 @@ const {
 } = require('firebase/firestore');
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJh45lbjhCek0BP30K7YvSK-hG-8SeWa0',
-  authDomain: 'cards-against-humanity-16605.firebaseapp.com',
-  projectId: 'cards-against-humanity-16605',
-  storageBucket: 'cards-against-humanity-16605.appspot.com',
-  messagingSenderId: '1094359847041',
-  appId: '1:1094359847041:web:a8ecc0bd327d85ee5f3d80',
-  measurementId: 'G-2GNVB3MBHM',
+  apiKey: "AIzaSyDBHN9vw2-fL0dO4D1A7afJORIEpptEc3U",
+  authDomain: "morsecah.firebaseapp.com",
+  projectId: "morsecah",
+  storageBucket: "morsecah.appspot.com",
+  messagingSenderId: "385232562142",
+  appId: "1:385232562142:web:5bafe1d6324428363a1f4f",
+  measurementId: "G-EQVRWNENFK"
 };
 
 const app = initializeApp(firebaseConfig);
